a83eff1167fca25ce2bdd9e1279ab79c9f506f7f
wooyong99 <wulovesk@naver.com>
2024-08-29 11:04:57 +0900

    feat : 커스텀 사용자 정보 (UserDetails) 구현 (#1)

---
+++ src/main/java/com/example/board/global/security/CustomDetails.java
@@ -0,0 +1,40 @@
+package com.example.board.global.security;
+
+import com.example.board.domain.member.entity.Member;
+import java.util.ArrayList;
+import java.util.Collection;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class CustomDetails implements UserDetails {
+
+    private Member member;
+
+    public CustomDetails(Member member) {
+        this.member = member;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collect = new ArrayList<>();
+        collect.add(new GrantedAuthority() {
+            @Override
+            public String getAuthority() {
+                return member.getRole().toString();
+            }
+        });
+        return collect;
+    }
+
+    @Override
+    public String getPassword() {
+        return member.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return member.getEmail();
+    }
+
+
+}
