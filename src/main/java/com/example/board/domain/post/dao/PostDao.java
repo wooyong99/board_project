db008f7a1517b88823719736310ac5d9e7a6e10f
wooyong99 <wulovesk@naver.com>
2024-08-30 14:29:12 +0900

    feat : PostDao - 게시글 리스트 조회에 대한 동적 쿼리 구현 (#9)

---
+++ src/main/java/com/example/board/domain/post/dao/PostDao.java
@@ -0,0 +1,17 @@
+package com.example.board.domain.post.dao;
+
+import com.example.board.domain.post.dto.PostListResponse;
+import com.example.board.domain.post.entity.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostDao extends JpaRepository<Post, Long>, CustomPostDao {
+
+    Page<Post> findByCategoryIdOrderByCreatedAtDesc(Long categoryId, Pageable pageable);
+
+    Page<Post> findAllByOrderByCreatedAtDesc(Pageable pageable);
+
+    @Override
+    Page<PostListResponse> findPostList(Long categoryId, Pageable pageable);
+}
