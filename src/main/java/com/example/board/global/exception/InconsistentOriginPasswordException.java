a0b7390f62171c5c6bad4d2e33153afd5a4da9db
wooyong99 <wulovesk@naver.com>
2024-08-30 09:59:50 +0900

    feat : MemberService 비밀번호 변경 기능 구현 (#1)

---
+++ src/main/java/com/example/board/global/exception/InconsistentOriginPasswordException.java
@@ -0,0 +1,8 @@
+package com.example.board.global.exception;
+
+public class InconsistentOriginPasswordException extends RuntimeException {
+
+    public InconsistentOriginPasswordException(String msg) {
+        super(msg);
+    }
+}
