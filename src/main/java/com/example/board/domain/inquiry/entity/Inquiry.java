c0334fb0f3bc892e2c108bec0d9512e301bdcc34
wooyong99 <wulovesk@naver.com>
2024-08-30 10:15:56 +0900

    feat : 문의 Entity 생성 (#1)

---
+++ src/main/java/com/example/board/domain/inquiry/entity/Inquiry.java
@@ -0,0 +1,35 @@
+package com.example.board.domain.inquiry.entity;
+
+import com.example.board.domain.member.entity.Member;
+import com.example.board.domain.model.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.ConstraintMode;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Inquiry extends BaseEntity {
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @OneToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(unique = true, nullable = false, foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private Member member;
+
+    @Builder
+    public Inquiry(String content, Member member) {
+        this.content = content;
+        this.member = member;
+    }
+
+}
