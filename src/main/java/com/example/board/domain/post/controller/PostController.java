ebe985a09308ca88cf39316985f5d34a9604859c
wooyong99 <wulovesk@naver.com>
2024-08-30 14:27:51 +0900

    feat : PostController - 게시글 리스트 조회 기능 구현 (#9)

--- src/main/java/com/example/board/domain/post/controller/PostController.java
+++ src/main/java/com/example/board/domain/post/controller/PostController.java
@@ -1,16 +1,21 @@
 package com.example.board.domain.post.controller;
 
 import com.example.board.domain.post.dto.PostCreateRequest;
+import com.example.board.domain.post.dto.PostListResponse;
 import com.example.board.domain.post.entity.Post;
 import com.example.board.domain.post.service.PostService;
 import java.security.Principal;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/posts")
@@ -18,6 +23,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class PostController {
 
     private final PostService postService;
+
+    // 게시글 리스트 조회
+    @GetMapping
+    public String postList(@RequestParam(required = false, name = "categoryId") Long categoryId,
+        @PageableDefault Pageable pageable, Model model) {
+        Page<PostListResponse> posts = postService.getPostList(categoryId, pageable);
+
+        model.addAttribute("posts", posts);
+        model.addAttribute("category", categoryId);
+
+        return "posts/list";
+    }
 
     // 게시글 등록 페이지 이동
     @GetMapping("/registerForm")
