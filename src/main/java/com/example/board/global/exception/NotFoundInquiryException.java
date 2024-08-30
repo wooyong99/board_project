2d92fa6240c3b46adb846d9d934969cea9a46e5c
wooyong99 <wulovesk@naver.com>
2024-08-30 10:19:32 +0900

    feat : 문의 데이터 조회 실패 Exception 객체 생성 (#1)

---
+++ src/main/java/com/example/board/global/exception/NotFoundInquiryException.java
@@ -0,0 +1,8 @@
+package com.example.board.global.exception;
+
+public class NotFoundInquiryException extends RuntimeException {
+
+    public NotFoundInquiryException(String msg) {
+        super(msg);
+    }
+}
