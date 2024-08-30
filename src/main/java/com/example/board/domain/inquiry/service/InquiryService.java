435191d83f5caf15187a16e8ab68344d2b4633a1
wooyong99 <wulovesk@naver.com>
2024-08-30 10:18:44 +0900

    feat : InquiryService - 문의 기능 구현 (#1)

---
+++ src/main/java/com/example/board/domain/inquiry/service/InquiryService.java
@@ -0,0 +1,47 @@
+package com.example.board.domain.inquiry.service;
+
+import com.example.board.domain.inquiry.dao.InquiryDao;
+import com.example.board.domain.inquiry.dto.InquiryCreateRequest;
+import com.example.board.domain.inquiry.entity.Inquiry;
+import com.example.board.domain.member.dao.MemberDao;
+import com.example.board.domain.member.entity.Member;
+import com.example.board.global.exception.DuplicateInquiryException;
+import com.example.board.global.exception.NotFoundMemberException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class InquiryService {
+
+    private final InquiryDao inquiryDao;
+    private final MemberDao memberDao;
+    private final PasswordEncoder encoder;
+
+    @Transactional
+    public void register(InquiryCreateRequest request) {
+        Member member = findMemberByEmail(request.getEmail());
+        if (encoder.matches(request.getPassword(), member.getPassword())) {
+            throw new NotFoundMemberException("존재하지 않는 사용자입니다.");
+        }
+        if (!member.isBlock()) {
+            throw new IllegalArgumentException("차단된 사용자가 아닙니다.");
+        }
+        if (inquiryDao.existsByMemberId(member.getId())) {
+            throw new DuplicateInquiryException("이미 차단 해제 문의 내역이 존재합니다.");
+        }
+        Inquiry inquiry = Inquiry.builder()
+            .content(request.getContent())
+            .member(member)
+            .build();
+        inquiryDao.save(inquiry);
+    }
+
+    private Member findMemberByEmail(String email) {
+        return memberDao.findByEmail(email).orElseThrow(
+            () -> new NotFoundMemberException("존재하지 않는 사용자입니다.")
+        );
+    }
+}
