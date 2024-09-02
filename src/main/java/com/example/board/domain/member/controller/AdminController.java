612c4f6a1d80c4aa96e974127192a7e9f961b2f5
wooyong99 <wulovesk@naver.com>
2024-09-02 14:27:59 +0900

    feat : AdminController - 사용자 검색 기능 구현 (#23)

--- src/main/java/com/example/board/domain/member/controller/AdminController.java
+++ src/main/java/com/example/board/domain/member/controller/AdminController.java
@@ -1,14 +1,20 @@
 package com.example.board.domain.member.controller;
 
+import com.example.board.domain.member.dto.MemberInfoResponse;
 import com.example.board.domain.member.dto.SignupRequest;
 import com.example.board.domain.member.entity.MemberRoleEnum;
 import com.example.board.domain.member.service.MemberService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
@@ -25,7 +31,20 @@ public class AdminController {
     @PostMapping("/doSignup")
     public String doSignup(@Valid SignupRequest signupRequest) {
         memberService.signup(signupRequest, MemberRoleEnum.ADMIN);
-        
+
         return "redirect:/posts";
+    }
+
+    // 회원 검색
+    @GetMapping("/search/members")
+    public String searchMembers(@RequestParam(required = false, name = "keyword") String keyword,
+        @PageableDefault Pageable pageable,
+        Model model) {
+
+        Page<MemberInfoResponse> members = memberService.search(keyword, pageable);
+
+        model.addAttribute("members", members);
+
+        return "admins/memberList";
     }
 }
